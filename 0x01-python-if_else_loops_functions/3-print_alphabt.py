3bd8b050970b36e42da019622e54470a168560d2
iddokip <kiptarusiddo@gmail.com>
2023-06-07 13:50:02 +0300

    print aphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter_code in range(97, 123):
     if chr(letter_code) not in ['e', 'q']:
-        print(chr(letter_code), end = "" )
+        print(chr(letter_code), end="")
