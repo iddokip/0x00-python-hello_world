9d4d1446f4a0c77addb8bde245577cce0bd10732
iddokip <kiptarusiddo@gmail.com>
2023-06-12 06:10:01 -0700

    maximum in list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    lenn = len(my_list)
+    for i in range(lenn - 1):
+        if my_list[i] > my_list[i + 1]:
+            return my_list[i]
