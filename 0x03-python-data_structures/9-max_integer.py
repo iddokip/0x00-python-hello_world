b098baaeb584064b47834ca2d22e4c3a7fd9e08a
iddokip <kiptarusiddo@gmail.com>
2023-06-12 06:18:06 -0700

    max intager

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     lenn = len(my_list)
-    for i in range(lenn - 1):
+    for i in range(lenn):
         if my_list[i] > my_list[i + 1]:
             return my_list[i]
