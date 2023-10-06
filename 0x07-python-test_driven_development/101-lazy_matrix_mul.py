7413d01b1a146b34ea6ceec1b4a0640150cfd0d7
IDDO <38156950+iddokip@users.noreply.github.com>
2023-10-06 13:24:42 +0300

    Delete 0x07-python-test_driven_development directory

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""A module that contains the function 'lazy_matrix_mul'"""
-import numpy as np
-
-
-def lazy_matrix_mul(m_a, m_b):
-    """returns the product of two conformable matrices in a lazy way"""
-    return np.matmul(m_a, m_b)
--- 0x07-python-test_driven_development/README.md
+++
@@ -1,2 +0,0 @@
-Projects done in the mastery journey to Test Driven Development. 
-Functions and files herein examplify TDD.
